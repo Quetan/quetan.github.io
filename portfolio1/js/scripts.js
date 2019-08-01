c2826b461dc52abdace0580088dcd1830991807b
Quetan <quetancraft@gmail.com>
2019-08-01 17:22:40 +0700

    v1.0
    Started.

---
+++ portfolio1/js/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById('answer_btn1').onclick = function() {
+    document.getElementById('answer_ptr1').classList.toggle('fa-rotate-90');
+    };
+
+    document.getElementById('answer_btn2').onclick = function() {
+    document.getElementById('answer_ptr2').classList.toggle('fa-rotate-90');
+    };
+
+    document.getElementById('answer_btn3').onclick = function() {
+    document.getElementById('answer_ptr3').classList.toggle('fa-rotate-90');
+    };
+
+    document.getElementById('answer_btn4').onclick = function() {
+    document.getElementById('answer_ptr4').classList.toggle('fa-rotate-90');
+    };
+
+});
