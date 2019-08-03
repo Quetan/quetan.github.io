9cdd2f80ccf79928a60f55c75815a9d15fa1e08f
Quetan <quetancraft@gmail.com>
2019-08-03 21:42:46 +0700

    v0.3
    Header.

--- portfolio3/js/_custom.js
+++ portfolio3/js/_custom.js
@@ -4,7 +4,59 @@ document.addEventListener("DOMContentLoaded", function() {
 	$('.hamburger').on('click',function(){
 		$(this).toggleClass('is-active');
 		$('.menu-fixed').toggleClass('is-active');
+		if ( $('.menu-fixed').attr('class') == "menu-fixed is-active" ){
+			$('.backdrop').css({"left":"0", "opacity":"1"});
+		}
+		else{
+			$('.backdrop').css({"left":"-100%", "opacity":"0"});
+		}	
 	});
+
+//Smooth scroll animation
+	$(".header-nav__item a").on("click", function() {	
+		var get_id = $(this).attr("data-item");
+		var target = $("#"+get_id).offset().top;	
+		$("html, body").animate({scrollTop: target}, 800);
+		});
+
+	$(".header__fixed-nav-item a").on("click", function() {
+
+			var get_id = $(this).attr("data-item");
+			var target = $("#"+get_id).offset().top;
+	
+			$("html, body").animate({scrollTop: target}, 800);
+	});
+
+//Active menu item
+
+	$(window).scroll(function(){
+		var $sections = $('section');
+		$sections.each(function(i,el){
+			var top  = $(el).offset().top-160;
+			var bottom = top +$(el).height();
+			var scroll = $(window).scrollTop();
+			var id = $(el).attr('id');
+
+			if(scroll > top && scroll < bottom){
+				$('a.is-active').removeClass('is-active');
+				$('a[href="#'+id+'"]').addClass('is-active');
+			}
+			else{
+				$('a[href="#'+id+'"]').removeClass('is-active');
+			}
+		});
+	});
+
+
+// Dark topline
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > $(this).height()/2){
+				$('.header__topline').addClass('header__dark');
+		}
+		else {
+				$('.header__topline').removeClass('header__dark');
+		};
+});
 
 
 });
