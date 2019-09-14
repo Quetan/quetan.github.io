c7044e24149ed374d9b0d2c84ef27a588689b6d8
Quetan <quetancraft@gmail.com>
2019-09-15 05:16:05 +0700

    v2.0

--- portfolio2/js/main.js
+++ portfolio2/js/main.js
@@ -19,6 +19,60 @@ $(document).ready(function() {
     }
   });
 
+  //********** lazy loading map
+  var loaded = false;
+  $(document).on("scroll", function() {
+    var offset = $(".map-area").offset().top - 250;
+    if ($(window).scrollTop() > offset && !loaded) {
+      loaded = true;
+      $("#mapid").show();
+      //создаем карту с центром в Барнауле
+      let mymap = L.map("mapid", {
+        scrollWheelZoom: false
+      }).setView([53.34795462, 83.77565922], 13);
+      L.tileLayer(
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox.streets",
+          accessToken:
+            "pk.eyJ1Ijoic3Bpcml0MDRlayIsImEiOiJjanZwNmtreTcxZHkyNGF2bmpocGlvbzh2In0.Lu2brpXppB3r8HrVUOdt8g"
+        }
+      ).addTo(mymap); //Добавляем дефолтные слои на карту
+      //(function() {
+      //     let coordinatesData = document.getElementById("coordinatesData").innerHTML;
+      //     let myCoordinatesData = JSON.parse(coordinatesData);
+      //     let obj = {
+      //         lat: 0,
+      //         lng: 0
+      //     };
+      //     for (let i = 0; i < myCoordinatesData.length; i++) {
+      //         let str = myCoordinatesData[i].coordinates.split(';');
+      //         obj.lat = str[0].replace(/(\w+):/, "");
+      //         obj.lng = str[1].replace(/(\w+):/, "");
+      //         marker = new L.Marker([obj.lat, obj.lng], {
+      //             draggable: false
+      //         });
+      //         circle = new L.circle([obj.lat, obj.lng], {
+      //             radius: 400
+      //         });
+      //         mymap.addLayer(marker);
+      //         // mymap.addLayer(circle);
+      //     }
+
+      //     mymap.on('click', function() {
+      //         if (mymap.scrollWheelZoom.enabled()) {
+      //             mymap.scrollWheelZoom.disable();
+      //         } else {
+      //             mymap.scrollWheelZoom.enable();
+      //         }
+      //     });
+      // })();
+    }
+  });
+
   //********** menu hides after click (mobile menu)
 
   $(document).on("click", ".navbar-collapse.in", function(e) {
