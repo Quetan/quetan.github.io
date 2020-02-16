c5449e02e15fa817739e989e5a6ba7e15a76d9fe
Sergey Alhimenko <seregaalhimenko@gmail.com>
2020-02-16 12:37:40 +0700

    Autoprefiex css. Delete unused js.  Fixed mobile version. Add active menu item listener.

--- forum2/js/main.js
+++ forum2/js/main.js
@@ -29,7 +29,65 @@ $(function() {
         enabled: true
     }
   });
+
+
+  //smooth scrolling
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .not('[href="#track1"]')
+  .not('[href="#track2"]')
+  .not('[href="#track3"]')
+  .not('[href="#track4"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
   
+});
+
+//Active links
+const clean_active_classes = items => {
+  items.forEach(item => {
+    if (item.classList.contains("active")) item.classList.remove("active");
+  })
+};
+
+let nav_items = document.querySelectorAll(".nav-item");
+  nav_items.forEach(item => {
+    item.addEventListener("click", () => {
+      clean_active_classes(nav_items);
+      item.classList.add("active");
+  });
 });
 
 
