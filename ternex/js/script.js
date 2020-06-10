5751bb2546be292786f206fa70143749eeda8290
quetan <quetancraft@gmail.com>
2020-06-10 20:09:02 +0700

    Minor changes.

--- ternex/js/script.js
+++ ternex/js/script.js
@@ -218,7 +218,7 @@ $(document).ready(function() {
         var name = $("#name").val();
         var emaild = $("#email").val();
         var subject = $("#subject").val();
-        var message = $("#message").val();
+        var phone = $("#phone").val();
         var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
         if (!name) {
             $(".form_error .name_error").addClass("show").removeClass("hide");
@@ -238,38 +238,38 @@ $(document).ready(function() {
                 return false;
             }
         }
-        if (!message) {
+        if (!phone) {
             $(".form_error .message_error").addClass("show").removeClass("hide");
             return false;
         } else {
             $(".form_error .message_error").addClass("hide").removeClass("show");
         }
-        if (name && emaild && message) {
-            $.ajax({
-                url: 'contact.php',
-                data: {
-                    name: name,
-                    emaild: emaild,
-                    subject: subject,
-                    message: message
-                },
-                type: 'POST',
-                success: function(data) {
-                    $(".Sucess").show();
-                    $(".Sucess").fadeIn(2000);
-                    $(".Sucess").html("<i class='fa fa-check'></i> Dear <b>" + name + "</b> Thank you for your inquiry we will respond to you as soon as possible!");
-                    $("#Name").val("");
-                    $("#Email").val("");
-                    $("#Subject").val("");
-                    $("#Message").val("");
-                    $(".form_error .name_error, .form_error .email_error, .form_error .email_val_error, .form_error .message_error").addClass("hide").removeClass("show");
-                    $("#name").val("");
-                    $("#email").val("");
-                    $("#subject").val("");
-                    $("#message").val("");
-                }
-            });
-        }
+        // if (name && emaild && phone) {
+        //     $.ajax({
+        //         url: 'contact.php',
+        //         data: {
+        //             name: name,
+        //             emaild: emaild,
+        //             subject: subject,
+        //             message: message
+        //         },
+        //         type: 'POST',
+        //         success: function(data) {
+        //             $(".Sucess").show();
+        //             $(".Sucess").fadeIn(2000);
+        //             $(".Sucess").html("<i class='fa fa-check'></i> Dear <b>" + name + "</b> Thank you for your inquiry we will respond to you as soon as possible!");
+        //             $("#Name").val("");
+        //             $("#Email").val("");
+        //             $("#Subject").val("");
+        //             $("#Message").val("");
+        //             $(".form_error .name_error, .form_error .email_error, .form_error .email_val_error, .form_error .message_error").addClass("hide").removeClass("show");
+        //             $("#name").val("");
+        //             $("#email").val("");
+        //             $("#subject").val("");
+        //             $("#message").val("");
+        //         }
+        //     });
+        // }
         return false;
     });
 });
