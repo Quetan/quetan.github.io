b72b30b624205354a27168f70665c0167fa0000b
quetan <quetancraft@gmail.com>
2021-01-23 04:52:47 +0700

    Add FINIKO

---
+++ finiko/js/contact.js
@@ -0,0 +1,38 @@
+/* Template Name: Delvy - Landing
+   Author: Themesbrand
+   Version: 1.0.0
+   Created: Sep 2020
+   File Description: Contact Form
+*/
+
+$('#contact-form').submit(function() {
+    var action = $(this).attr('action');
+    $("#message").slideUp(750, function() {
+        $('#message').hide();
+
+        $('#submit')
+            .before('')
+            .attr('disabled', 'disabled');
+
+        $.post(action, {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                comments: $('#comments').val(),
+            },
+            function(data) {
+                document.getElementById('message').innerHTML = data;
+                $('#message').slideDown('slow');
+                $('#cform img.contact-loader').fadeOut('slow', function() {
+                    $(this).remove()
+                });
+                $('#submit').removeAttr('disabled');
+                if (data.match('success') != null) $('#cform').slideUp('slow');
+            }
+        );
+
+    });
+
+    return false;
+
+});
+
