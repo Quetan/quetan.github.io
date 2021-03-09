d5da97af82b1699761460183de075798af5429e0
quetan <quetancraft@gmail.com>
2021-03-09 13:45:08 +0700

    Add countdown to new file, fix online pay button.

--- abiturient/js/main.js
+++ abiturient/js/main.js
@@ -76,30 +76,6 @@ $(".announces-carousel").owlCarousel({
   },
 });
 
-// countdown script
-function countDown() {
-  var distance =
-    new Date("Jun 18, 2021 00:00:00").getTime() - new Date().getTime();
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  let daysWrapper = document.getElementById("countdown-days");
-  daysWrapper = daysWrapper.innerHTML = days + " дн";
-  let hoursWrapper = document.getElementById("countdown-hours");
-  hoursWrapper = hoursWrapper.innerHTML = hours + " ч";
-  let minutesWrapper = document.getElementById("countdown-minutes");
-  minutesWrapper = minutesWrapper.innerHTML = minutes + " мин";
-  let secsWrapper = document.getElementById("countdown-secs");
-  secsWrapper = secsWrapper.innerHTML = seconds + " сек";
-  if (distance < 0) {
-    clearInterval(timer);
-    document.querySelector(".countdown-items").innerHTML =
-      "Приемная комиссия ужа стартовала!";
-  }
-}
-let timer = setInterval(countDown, 1000);
-
 // YOUTUBE VIDEO PLACEHOLDER
 
 (function () {
