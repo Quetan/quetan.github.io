413e96b57338575475e6746c48ea6a1054fb7d4b
Quetan <quetancraft@gmail.com>
2022-10-18 18:05:22 +0700

    MediaHub initial commit

---
+++ Vue/lessons-cli/src/components/blogPost.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>{{ formatTitle }}</h2>
+  <openBlogLink :link="link" />
+</template>
+
+<script>
+import openBlogLink from './openBlogLink.vue';
+
+export default {
+  name: 'BlogPost',
+  components: {
+    openBlogLink
+  },
+  props: {
+    title: String,
+    link: String,
+  },
+  computed: {
+    formatTitle() {
+      if (this.title.length > 0) {
+        return 'Запись - «' + this.title + '»'
+      } else {
+        return 'Заголовок отсутствует...'
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ Vue/lessons-cli/src/components/openBlogLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <button v-show="link" v-on:click="openPage()">Перейти</button>
+</template>
+
+<script>
+export default {
+    name: 'OpenBlogLink',
+    props: {
+        link: String,
+    },
+    methods: {
+        openPage() {
+            window.open(this.link, "_blank")
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
