f8a0874c9e5bde40f019796fa48bb1db78e20766
Quetan <quetancraft@gmail.com>
2023-10-17 16:43:19 +0700

    Add news to rnd

--- rnd/js/main.js
+++ rnd/js/main.js
@@ -21,3 +21,35 @@ let controls = [...navLinks, closeMobileNavigation];
 controls.forEach((c) => {
 	c.addEventListener("click", hideMobileNavigation);
 });
+
+const counter = (duration = 2000) => {
+	const numbers = document.querySelectorAll("[data-counter]");
+	numbers.forEach((number) => {
+		let from = Number(number.dataset.counterFrom);
+		const to = Number(number.dataset.counterTo);
+		const step = to > from ? 1 : -1;
+		const interval = duration / to;
+
+		if (from === to) {
+			number.textContent = from;
+			return;
+		}
+
+		const counter = setInterval(() => {
+			from += step;
+			number.innerHTML = `${from}<sup>+</sup>`;
+			if (from === to) {
+				clearInterval(counter);
+			}
+		}, interval);
+	});
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+	const options = {
+		rootMargin: "0px 0px 75px 0px",
+		threshold: 1,
+	};
+	const numbersObserver = new IntersectionObserver(counter, options);
+	numbersObserver.observe(document.querySelector("#numbers"));
+});
