f46178d9f024f96cace254484a1c37c17cf60249
Quetan <quetancraft@gmail.com>
2024-02-16 11:44:28 +0700

    Add mobile menu. Fix layout shifts on mobile devices.

--- prof/src/main.ts
+++ prof/src/main.ts
@@ -20,6 +20,30 @@ const PRIM_GROUP: HTMLButtonElement | null = document.querySelector('#PRIM_GROUP
 const PRIM_SEMINAR: HTMLButtonElement | null = document.querySelector('#PRIM_SEMINAR');
 const PRIM_MEDIA: HTMLButtonElement | null = document.querySelector('#PRIM_MEDIA');
 
+const MOBILE_NAV_MENU: HTMLDivElement | null = document.querySelector('#nav-mobile-menu');
+const MOBILE_NAV_BUTTON: HTMLButtonElement | null = document.querySelector('#nav-mobile-button');
+const MOBILE_NAV_BUTTON_CLOSE: HTMLDivElement | null = document.querySelector(
+	'#nav-mobile-button-close'
+);
+
+const openMobileMenu = () => {
+	if (!MOBILE_NAV_MENU) return null;
+
+	MOBILE_NAV_MENU.classList.remove('close');
+	MOBILE_NAV_MENU.classList.add('open');
+};
+
+const closeMobileMenu = () => {
+	if (!MOBILE_NAV_MENU) return null;
+	MOBILE_NAV_MENU.classList.remove('open');
+	MOBILE_NAV_MENU.classList.add('close');
+};
+
+MOBILE_NAV_BUTTON?.addEventListener('click', openMobileMenu);
+MOBILE_NAV_BUTTON_CLOSE?.addEventListener('click', closeMobileMenu);
+
+MOBILE_NAV_MENU?.childNodes.forEach(item => item.addEventListener('click', closeMobileMenu));
+
 type Category = 'all' | 'ind' | 'group' | 'seminar' | 'media';
 
 const queryString = window.location.search;
