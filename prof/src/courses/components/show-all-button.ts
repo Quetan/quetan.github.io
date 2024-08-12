5f4e41c87bee64f61c988c30ae25d0f2b1d3fd3a
Quetan <quetancraft@gmail.com>
2024-08-12 14:02:08 +0700

    Fix relative path.

--- prof/src/courses/components/show-all-button.ts
+++ prof/src/courses/components/show-all-button.ts
@@ -2,7 +2,7 @@ import { Category } from '../../interfaces';
 
 const drawShowAllButton = (courseType: Category) => {
 	return `
-    <a href="./courses.html?type=${courseType}" style="grid-column: 1 / 4;" class="button button-md mx-auto mt-8 block ">
+    <a href="/courses.html?type=${courseType}" style="grid-column: 1 / 4;" class="button button-md mx-auto mt-8 block ">
         Смотреть все
     </a>
     `;
